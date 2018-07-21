ecb22947fe967bb16a2af66ed351f11f19c3c537
Aleksey <lesha.leshka@inbox.ru>
2018-07-21 16:20:27 +0300

    Testing collaborators

---
+++ app/Team.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Team extends Model
+{
+    protected $fillable = ['name', 'size'];
+
+    public function add($user)
+    {
+        $this->guardAgainstTooManyMembers();
+
+        $method = $user instanceof User ? 'save' : 'saveMany';
+
+        $this->members()->$method($user);
+    }
+
+    public function remove($users = null)
+    {
+        if ($users instanceof User) {
+            return $users->leaveTeam();
+        }
+
+        return $this->removeMany($users);
+    }
+
+    public function removeMany($users)
+    {
+        return $this->members()
+                    ->whereIn('id', $users->pluck('id'))
+                    ->update(['team_id' => null]);
+    }
+
+    public function restart()
+    {
+        return $this->members()->update(['team_id' => null]);
+    }
+
+    public function members()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function count()
+    {
+        return $this->members()->count();
+    }
+
+    public function guardAgainstTooManyMembers()
+    {
+        if ($this->count() >= $this->size) {
+            throw new \Exception;
+        }
+    }
+}
