0c73c321e68696fadbcc4aa2e0310454ddab318e
Aleksey <lesha.leshka@inbox.ru>
2018-07-22 17:21:43 +0300

    Test method refactoring

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,15 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function signIn($user = null)
+    {
+        $user = $user ?: factory(\App\User::class)->create();
+
+        $this->user = $user;
+
+        $this->actingAs($user);
+
+        return $this;
+    }
 }
